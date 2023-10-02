d1cb8e2c83f99b4c1b278585d587689fdf1bebc5
Ashton Snelgrove <ashton@snelgrove.science>
2023-10-02 13:24:00 -0600

    Lab 5 fix sleep code which was a copy of the timer rather than the sleep.

--- labs/5.realtime/reference/src/sleep.c
+++ labs/5.realtime/reference/src/sleep.c
@@ -12,11 +12,8 @@ void sleep_main(void)
     dev_out = device_get_binding(DT_LABEL(DEV_OUT));
     gpio_pin_configure(dev_out, PIN_OUT, GPIO_OUTPUT_ACTIVE);
 
-	struct k_timer t;
-	k_timer_init(&t, NULL, NULL);
     while (1) {
         gpio_pin_toggle(dev_out, PIN_OUT);
-		k_timer_start(&t, K_MSEC(1), K_NO_WAIT);
-		k_timer_status_sync(&t);
+        k_sleep(K_MSEC(1));
     }
 }
