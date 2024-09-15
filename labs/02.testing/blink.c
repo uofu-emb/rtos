28e1b2f210fac4e6dd5a6ec967b70db43b9a93e6
Ashton Snelgrove <ashton@snelgrove.science>
2024-09-14 21:02:37 -0600

    Lab 02 Update the final revision of the example code.

--- labs/02.testing/blink.c
+++ labs/02.testing/blink.c
@@ -1,31 +1,54 @@
+/**
+ * Copyright (c) 2022 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
 #include <stdio.h>
+
+#include "FreeRTOS.h"
+#include "task.h"
+
 #include "pico/stdlib.h"
+#include "pico/multicore.h"
+#include "pico/cyw43_arch.h"
 
-#define PICO_LED_PIN 7
+int count = 0;
+bool on = false;
 
-int led_is_on;
-int count;
-int main( void )
-{
-    stdio_init_all();
-    printf("Starting up\n");
-    led_is_on = 0;
-    count = 0;
+#define MAIN_TASK_PRIORITY      ( tskIDLE_PRIORITY + 1UL )
+#define BLINK_TASK_PRIORITY     ( tskIDLE_PRIORITY + 2UL )
+#define MAIN_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
+#define BLINK_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
+
+void blink_task(__unused void *params) {
+    hard_assert(cyw43_arch_init() == PICO_OK);
+    while (true) {
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, on);
+        if (count++ % 11) on = !on;
+        vTaskDelay(500);
+    }
+}
+
+void main_task(__unused void *params) {
+    xTaskCreate(blink_task, "BlinkThread",
+                BLINK_TASK_STACK_SIZE, NULL, BLINK_TASK_PRIORITY, NULL);
     char c;
-    gpio_init(PICO_LED_PIN);
-    gpio_set_dir(PICO_LED_PIN, GPIO_OUT);
     while(c = getchar()) {
-        led_is_on = !led_is_on;
-        gpio_put(PICO_LED_PIN, led_is_on);
         if (c <= 'z' && c >= 'a') putchar(c - 32);
         else if (c >= 'A' && c <= 'Z') putchar(c + 32);
         else putchar(c);
-
-        if (count++ % 42) {
-            led_is_on = !led_is_on;
-            putchar('4');
-            putchar('2');
-        }
     }
+}
+
+int main( void )
+{
+    stdio_init_all();
+    const char *rtos_name;
+    rtos_name = "FreeRTOS";
+    TaskHandle_t task;
+    xTaskCreate(main_task, "MainThread",
+                MAIN_TASK_STACK_SIZE, NULL, MAIN_TASK_PRIORITY, &task);
+    vTaskStartScheduler();
     return 0;
 }
